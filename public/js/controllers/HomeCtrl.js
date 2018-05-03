8dc0f4eb7231d475dd7b92a08e3d5723f2ddad60
Mohammed Alkhateeb <mohmmad.alkhateeb97.ma@gmail.com>
2018-05-03 10:51:40 +0200

    files

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -21,6 +21,52 @@ app.controller('HomeCtrl', function ($scope, $firebaseArray) {
                 latitude: pos.coords.latitude,
                 longitude: pos.coords.longitude
             };
+            var geojson = {
+                "type": "FeatureCollection",
+                "features": [
+                   
+                    {
+                        "type": "Feature",
+                        "properties": {
+                            "message": "Baz",
+                            "iconSize": [19, 22]
+                        },
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [
+                                12.6914093,
+                                56.0573607
+                            ]
+                        }
+                    }
+                ]
+            };
+            
+            var map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mezar/cjgp399qn00bc2rp3gjqgyqiy',
+                center: [location.latitude,location.longitude],
+                zoom: 16
+            });
+            
+            // add markers to map
+            geojson.features.forEach(function(marker) {
+                // create a DOM element for the marker
+                var el = document.createElement('div');
+                el.className = 'mapboxgl-user-location-dot';
+                el.style.backgroundColor='#1da1f2';
+                el.style.width = marker.properties.iconSize[0] + 'px';
+                el.style.height = marker.properties.iconSize[1] + 'px';
+            
+                el.addEventListener('click', function() {
+                    window.alert(marker.properties.message);
+                });
+            
+                // add marker to map
+                new mapboxgl.Marker(el)
+                    .setLngLat(marker.geometry.coordinates)
+                    .addTo(map);
+            });
             users.$add(location)
                 .then(function (ref) {
                     var id = ref.key;
@@ -48,52 +94,7 @@ app.controller('HomeCtrl', function ($scope, $firebaseArray) {
         return returnArr;
     };
 
-    var geojson = {
-        "type": "FeatureCollection",
-        "features": [
-           
-            {
-                "type": "Feature",
-                "properties": {
-                    "message": "Baz",
-                    "iconSize": [19, 22]
-                },
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [
-                        12.6914093,
-                        56.0573607
-                    ]
-                }
-            }
-        ]
-    };
     
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mezar/cjgp399qn00bc2rp3gjqgyqiy',
-        center: [12.6914093,56.0573607],
-        zoom: 16
-    });
-    
-    // add markers to map
-    geojson.features.forEach(function(marker) {
-        // create a DOM element for the marker
-        var el = document.createElement('div');
-        el.className = 'mapboxgl-user-location-dot';
-        el.style.backgroundColor='#1da1f2';
-        el.style.width = marker.properties.iconSize[0] + 'px';
-        el.style.height = marker.properties.iconSize[1] + 'px';
-    
-        el.addEventListener('click', function() {
-            window.alert(marker.properties.message);
-        });
-    
-        // add marker to map
-        new mapboxgl.Marker(el)
-            .setLngLat(marker.geometry.coordinates)
-            .addTo(map);
-    });
 
 
 
