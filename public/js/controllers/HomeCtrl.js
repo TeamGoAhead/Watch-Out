d7410defbe87bb36797e1d07f9d6a57551cc230c
Mohammed Alkhateeb <mohmmad.alkhateeb97.ma@gmail.com>
2018-05-03 10:15:22 +0200

    added files

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -2,6 +2,14 @@ var app = angular.module('watchOut');
 
 app.controller('HomeCtrl', function ($scope, $firebaseArray) {
 
+
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoibWV6YXIiLCJhIjoiY2pnZzh5amMyNDVidjJ3bGlveGEyeDZxaSJ9.eV1srEZNonNXeljsG18mww';
+
+
+
+
+
     var ref = firebase.database().ref().child('locations');
     var users = $firebaseArray(ref);
 
@@ -39,5 +47,56 @@ app.controller('HomeCtrl', function ($scope, $firebaseArray) {
 
         return returnArr;
     };
+
+    var geojson = {
+        "type": "FeatureCollection",
+        "features": [
+           
+            {
+                "type": "Feature",
+                "properties": {
+                    "message": "Baz",
+                    "iconSize": [19, 22]
+                },
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": [
+                        12.6914093,
+                        56.0573607
+                    ]
+                }
+            }
+        ]
+    };
+    
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mezar/cjgp399qn00bc2rp3gjqgyqiy',
+        center: [12.6914093,56.0573607],
+        zoom: 16
+    });
+    
+    // add markers to map
+    geojson.features.forEach(function(marker) {
+        // create a DOM element for the marker
+        var el = document.createElement('div');
+        el.className = 'mapboxgl-user-location-dot';
+        el.style.backgroundColor='#1da1f2';
+        el.style.width = marker.properties.iconSize[0] + 'px';
+        el.style.height = marker.properties.iconSize[1] + 'px';
+    
+        el.addEventListener('click', function() {
+            window.alert(marker.properties.message);
+        });
+    
+        // add marker to map
+        new mapboxgl.Marker(el)
+            .setLngLat(marker.geometry.coordinates)
+            .addTo(map);
+    });
+
+
+
+
 
 });
